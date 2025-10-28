308b0c84bcd242d78e60e9de403c6a15a4e4e2e1
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2025-10-28 09:52:44 +0000

    Add biome linter, lint all files

--- src/components/ui/Background.tsx
+++ src/components/ui/Background.tsx
@@ -4,44 +4,44 @@ import React, { useEffect, useState } from "react";
 import Beams from "../Beams";
 
 interface BackgroundProps extends React.HTMLProps<HTMLDivElement> {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 export const Background = ({
-  className,
-  children,
-  ...props
+	className,
+	children,
+	...props
 }: BackgroundProps) => {
-  const [isLoaded, setIsLoaded] = useState(false);
+	const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
-    setIsLoaded(true);
-  }, []);
+	useEffect(() => {
+		setIsLoaded(true);
+	}, []);
 
-  return (
-    <main>
-      <div
-        className={cn(
-          "transition-bg relative min-h-screen bg-background text-foreground transition-opacity duration-1400 ease-in-out",
-          isLoaded ? "opacity-100" : "opacity-0",
-          className,
-        )}
-        {...props}
-      >
-        <div className="absolute inset-0 overflow-hidden">
-          <Beams
-            beamWidth={1.2}
-            beamHeight={30}
-            beamNumber={12}
-            lightColor="#FFCA0A"
-            speed={2}
-            noiseIntensity={1.75}
-            scale={0.2}
-            rotation={30}
-          />
-        </div>
-        {children}
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div
+				className={cn(
+					"transition-bg relative min-h-screen bg-background text-foreground transition-opacity duration-1400 ease-in-out",
+					isLoaded ? "opacity-100" : "opacity-0",
+					className,
+				)}
+				{...props}
+			>
+				<div className="absolute inset-0 overflow-hidden">
+					<Beams
+						beamWidth={1.2}
+						beamHeight={30}
+						beamNumber={12}
+						lightColor="#FFCA0A"
+						speed={2}
+						noiseIntensity={1.75}
+						scale={0.2}
+						rotation={30}
+					/>
+				</div>
+				{children}
+			</div>
+		</main>
+	);
 };
