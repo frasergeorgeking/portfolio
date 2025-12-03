c6cefc21a3c154ecb207dc9b70bf432a4191ef7e
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2025-12-03 17:07:50 +0000

    Add lint command, apply feedback

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
-import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
+// import { defineCollection, z } from "astro:content";
+// import { glob } from "astro/loaders";
 
 // TODO FK: Define content collections when needed.
 export const collections = {
